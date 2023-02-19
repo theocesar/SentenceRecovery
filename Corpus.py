ec2aae812225592a5193f79fac4a9c8234664356
theocesar <theocesar@live.com>
2023-02-19 10:53:25 -0300

    Update Corpus.py

--- Corpus.py
+++ Corpus.py
@@ -1,9 +1,7 @@
 """
 // Théo César Zanotto da Silva
 
-ENUNCIADO
-
-Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de
+O algoritmo deverá transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de
 linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função
 que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta
 url. Duas condições são importantes:
@@ -11,7 +9,7 @@ url. Duas condições são importantes:
 a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que
 1000 palavras.
 
-b) O texto desta página deverá ser transformado em um array de senteças.
+b) O texto desta página deverá ser transformado em um array de sentenças.
 
 Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca
 Spacy.
@@ -25,6 +23,7 @@ import re
 
 array = []
 
+# chosen urls
 urls = ('https://hbr.org/2022/04/the-power-of-natural-language-processing',
         'https://monkeylearn.com/natural-language-processing/',
         'https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html',
@@ -33,18 +32,23 @@ urls = ('https://hbr.org/2022/04/the-power-of-natural-language-processing',
 
 
 def corpus(link):
+    # obtaining the content of the page
     page = urlopen(link).read()
     soup = BeautifulSoup(page, 'html.parser')
 
+    # removing the unnecessary tags
     for script in soup(["script", "style", "footer", "header"]):
         script.decompose()
 
+    # compacting all the data
     textof = ' '.join(soup.stripped_strings)
 
+    # splitting the text in sentences
     textof = re.split("[!?:;.]", textof)
 
     return textof
 
 
+# printing the resulting array.
 for url in urls:
     print(corpus(url))
